0dc2f4f3776cfdef683b7aa6b617a1f93bc8b0d2
Cain-Drouin <124199333+Cain-Drouin@users.noreply.github.com>
2023-02-15 14:07:11 -0500

    Abstract shape that translates

---
+++ CainLab3EX1.js
@@ -0,0 +1,24 @@
+function setup() {
+  createCanvas(400, 400);
+
+  angleMode(DEGREES);
+}
+
+function draw() {
+  background(100, 220, 95);
+
+  if (mouseX > 100) {
+    translate(100, 150);
+ 
+  } else {fill(90, 10, 45);}
+  noStroke();
+  rect(10, 10, 40);
+  rect(10, 80, 40);
+  push()
+  noFill();
+  stroke(90, 10, 45);
+  strokeWeight(10);
+  arc(40, 65, 100, 100, 270, 90);
+  pop()
+         
+}
